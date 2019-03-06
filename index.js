979d3f36304ba360d24ae470729be2e1b658b23b
Jose Mario Gutierrez <jogutierrez@H7689.ad.hugeinc.com>
2019-03-06 18:36:23 -0500

    Initial setup for speech recognition

---
+++ index.js
@@ -0,0 +1,46 @@
+window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+const synth = window.speechSynthesis;
+const recognition = new SpeechRecognition();
+
+const icon = document.querySelector('i.fa.fa-microphone')
+let paragraph = document.createElement('p');
+let container = document.querySelector('.text-box');
+
+container.appendChild(paragraph);
+const sound = document.querySelector('.sound');
+
+icon.addEventListener('click', () => {
+  const playPromise = sound.play();
+  if (playPromise !== undefined) {
+    playPromise.then(_ => {
+      // Automatic playback started!
+      // Show playing UI.
+      console.log('enter')
+    })
+    .catch(error => {
+      // Auto-play was prevented
+      // Show paused UI.
+    });
+  }
+  dictate();
+});
+
+const dictate = () => {
+  recognition.start();
+  recognition.onresult = (event) => {
+    const speechToText = event.results[0][0].transcript;
+    
+    paragraph.textContent = speechToText;
+
+    if (event.results[0].isFinal) {
+
+      if (speechToText.includes('mellow')) {
+          console.log('Respuesta correcta');
+          
+          utterThis = new SpeechSynthesisUtterance('Respuesta correcta');
+          synth.speak(utterThis);
+          window.location.href = "mello.html";
+      };
+    }
+  }
+}
